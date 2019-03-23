5456fd38d136ddd461d60e4581306b11d124ee8e
venu1320 <ajayjustin007@gmail.com>
2019-03-22 23:06:04 -0400

    water

---
+++ hungry1.py
@@ -0,0 +1,8 @@
+hungry=inpt("are you hungry?")
+if hungry=="yes":
+print("eat samosa")
+else:
+thirsty=input_"are you thirsty?")
+if thirsty=="yes":
+print("drink water")
+printf("do your home work")
